6e37c71c07eef7fdf6059b16f183c1cd409d39d5
samuel-adjin <samuel.adjin@amalitech.org>
2021-03-19 10:28:05 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// function deposit() {
+// let depo =[];
+// let amount = document.querySelector("#cash").value;
+// depo.push(Number(amount));
+// console.log(depo);
+
+    
+// }
+// document.querySelector(".btn1").addEventListener("click",deposit);
+
+
