ea8d551181b30717c65128b7129eaba7f3cb534c
samuel-adjin <samuel.adjin@amalitech.org>
2021-03-19 16:44:20 +0000

    js coded added

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
+
+var myarr = [];
+
 function deposit() {
 
 let amount = document.querySelector("#cash").value;
 let accountNum = document.querySelector("#num").value;
 let accountName = document.querySelector("#nam").value;
 
+
+
+ var total = 0;
+myarr.push(Number(document.querySelector("#cash").value));
+for(i=0; i<myarr.length;i++){
+    total =  myarr[i] + total;
+}
 alert("Deposit added Successfully");
-alert("Balance: " + amount + " " + "AccountNumber: " + accountNum +" "+ "AccountName: "+ accountName ); 
+alert("Balance: " + total + " " + "AccountNumber: " + accountNum +" "+ "AccountName: "+ accountName );
 }
 document.querySelector(".btn1").addEventListener("click",deposit);
+
 
 
